31b5cc929db1607fc8d6b3fcd7ba288d90c26ebc
DavidWindham <tinymouse2@gmail.com>
2023-01-26 00:47:20 +0000

    Modified proxy for auth to append x-api-key header for lambda auth

--- frontend/pages/api/auth/[...route].js
+++ frontend/pages/api/auth/[...route].js
@@ -1,9 +1,13 @@
 import httpProxyMiddleware from 'next-http-proxy-middleware';
 
+
 module.exports = (req, res) => {
     const { route } = req.query;
     httpProxyMiddleware(req, res, {
       target: process.env.AUTH_API_ADDRESS + route,
       pathRewrite: {'^/api/auth(/.*)?': ''},
+      headers: {
+        'x-api-key': process.env.AWS_GATEWAY_API_KEY
+      }
     });
 }
